7eadb79d9ad1fd7479796ccf0e6eb4824c4b6deb
matticbot <semantic-release-bot@martynus.net>
2024-11-11 17:10:36 +0000

    chore(release): 3.4.0-alpha.4 [skip ci]
    # [3.4.0-alpha.4](https://github.com/Automattic/newspack-newsletters/compare/v3.4.0-alpha.3...v3.4.0-alpha.4) (2024-11-11)
    
    ### Bug Fixes
    
    * remove behavior to hide post title in newsletter editor ([#1701](https://github.com/Automattic/newspack-newsletters/issues/1701)) ([8a15cf5](https://github.com/Automattic/newspack-newsletters/commit/8a15cf5393f6dcf6cbbe3a326d9e996610711649))

--- newspack-newsletters.php
+++ newspack-newsletters.php
@@ -8,7 +8,7 @@
  * License: GPL2
  * Text Domain:     newspack-newsletters
  * Domain Path:     /languages
- * Version:         3.4.0-alpha.3
+ * Version:         3.4.0-alpha.4
  *
  * @package         Newspack_Newsletters
  */
