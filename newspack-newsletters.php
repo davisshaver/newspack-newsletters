3b19813c7d6463229a5ca7f8dbdb053f46d53ad4
matticbot <semantic-release-bot@martynus.net>
2025-05-14 17:29:06 +0000

    chore(release): 3.13.0 [skip ci]
    # [3.13.0](https://github.com/Automattic/newspack-newsletters/compare/v3.12.2...v3.13.0) (2025-05-14)
    
    ### Bug Fixes
    
    * **admin-menu:** hide advertisers menu from admin dashboard ([#1820](https://github.com/Automattic/newspack-newsletters/issues/1820)) ([a88e198](https://github.com/Automattic/newspack-newsletters/commit/a88e198d83cb8677a300653daa0749bb658951e6))
    * **CC:** update existing contact when changing email address ([#1795](https://github.com/Automattic/newspack-newsletters/issues/1795)) ([c44d4d9](https://github.com/Automattic/newspack-newsletters/commit/c44d4d9e2d91be7a2f40177e90d95fd4c376d8ea))
    * **editor:** ads config call ([#1813](https://github.com/Automattic/newspack-newsletters/issues/1813)) ([14cfcf0](https://github.com/Automattic/newspack-newsletters/commit/14cfcf0c5399115d8ffc2f425a891debedfa2092))
    * **email-html:** button rendering ([#1812](https://github.com/Automattic/newspack-newsletters/issues/1812)) ([0fbb6f5](https://github.com/Automattic/newspack-newsletters/commit/0fbb6f5a295460e90ccc23eda15b4b9729a453a4))
    * **email-html:** sanity check for img alt attr ([#1804](https://github.com/Automattic/newspack-newsletters/issues/1804)) ([63d20e6](https://github.com/Automattic/newspack-newsletters/commit/63d20e69ee15e5b7f75d5ff0512506f586c44136))
    * **posts-inserter-block:** sponsorship scope handling ([#1802](https://github.com/Automattic/newspack-newsletters/issues/1802)) ([55e265a](https://github.com/Automattic/newspack-newsletters/commit/55e265ade7f0a5bd8fde54a380840f16517b5e19))
    * **posts-inserter:** use post date in TZ (instead of GMT) ([#1803](https://github.com/Automattic/newspack-newsletters/issues/1803)) ([60f1b77](https://github.com/Automattic/newspack-newsletters/commit/60f1b7765e673a515edcec838516cba39307cdf5))
    * revert [#1820](https://github.com/Automattic/newspack-newsletters/issues/1820) ([#1822](https://github.com/Automattic/newspack-newsletters/issues/1822)) ([0c8dea5](https://github.com/Automattic/newspack-newsletters/commit/0c8dea504349353b0612463040ce64761fd256fd))
    
    ### Features
    
    * disable Mailchimp notes ([#1816](https://github.com/Automattic/newspack-newsletters/issues/1816)) ([f3b3f94](https://github.com/Automattic/newspack-newsletters/commit/f3b3f9422334bb7928baa9e4b55aa3f6b4fcdb52))
    * **ga4:** dispatch newsletter_signup activity upon signup via block ([#1819](https://github.com/Automattic/newspack-newsletters/issues/1819)) ([548f1c3](https://github.com/Automattic/newspack-newsletters/commit/548f1c30b8b12cc3583b0f619dc482da177dde46))

--- newspack-newsletters.php
+++ newspack-newsletters.php
@@ -8,7 +8,7 @@
  * License: GPL2
  * Text Domain:     newspack-newsletters
  * Domain Path:     /languages
- * Version:         3.13.0-alpha.2
+ * Version:         3.13.0
  *
  * @package         Newspack_Newsletters
  */
