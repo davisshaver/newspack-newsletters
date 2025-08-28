ef8edb1cf199516705f9f7fb0782aa81ac80225f
matticbot <semantic-release-bot@martynus.net>
2025-08-28 20:02:19 +0000

    chore(release): 3.22.0-alpha.1 [skip ci]
    # [3.22.0-alpha.1](https://github.com/Automattic/newspack-newsletters/compare/v3.21.0...v3.22.0-alpha.1) (2025-08-28)
    
    ### Bug Fixes
    
    * **ads:** only apply click tracking to ad links ([#1895](https://github.com/Automattic/newspack-newsletters/issues/1895)) ([7d826c8](https://github.com/Automattic/newspack-newsletters/commit/7d826c8f95d582c361745118cc7613b915c5df46))
    * respect block spacing when rendering ([#1894](https://github.com/Automattic/newspack-newsletters/issues/1894)) ([8293690](https://github.com/Automattic/newspack-newsletters/commit/8293690828eed85f4dd0276c6f833154dd97cc0c))
    
    ### Features
    
    * add support for Foundation Community Remote Data Blocks ([#1898](https://github.com/Automattic/newspack-newsletters/issues/1898)) ([e5fd624](https://github.com/Automattic/newspack-newsletters/commit/e5fd62447a8c332d25340e1f6aed985a28516f14))
    * **layouts:** persist auto ads option ([#1896](https://github.com/Automattic/newspack-newsletters/issues/1896)) ([2b3ffac](https://github.com/Automattic/newspack-newsletters/commit/2b3ffacd663f8a997dc67e1830a9f8b765e72e99))

--- newspack-newsletters.php
+++ newspack-newsletters.php
@@ -8,7 +8,7 @@
  * License: GPL2
  * Text Domain:     newspack-newsletters
  * Domain Path:     /languages
- * Version:         3.21.0
+ * Version:         3.22.0-alpha.1
  *
  * @package         Newspack_Newsletters
  */
